be3d72db52ccd5864fd06ed9c5858a3c8ce88680
Junyeon <jun981008@naver.com>
2022-07-03 21:47:54 +0900

    reabBtn(changes)

--- javascript/readBtn.js
+++ javascript/readBtn.js
@@ -1,1 +1,14 @@
-const parentContainer = document.querySelector(".read-more-container");
+const btn = document.getElementById("read-more-btn");
+
+const changesStyle = document.querySelector(".container");
+
+function readBtn() {
+  if (changesStyle.style.display === "-webkit-box") {
+    changesStyle.style.display = "flex";
+  } else {
+    changesStyle.style.display = "-webkit-box";
+  }
+}
+
+readBtn();
+btn.addEventListener("click", readBtn);
