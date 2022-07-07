63c1f114de7b9776bfd1f482bb69cbeb1af706b3
Junyeon <jun981008@naver.com>
2022-07-07 11:38:03 +0900

    배너 자바스크립트 수정중

--- javascript/bannerToogleBtn.js
+++ javascript/bannerToogleBtn.js
@@ -5,42 +5,19 @@ toggleBtn.addEventListener("click", () => {
   bannerMenu.classList.toggle("active");
 });
 
-const connectCss = document.querySelector(".meun-container");
-const connectBanner = "coonect-banner";
+const menuContainer = document.querySelector(".menu-container");
+const menu = document.querySelector(".menu");
+const connectCss = "menu-connect";
 
-const path = window.location.pathname;
-const page = path.split("/").pop();
-console.log(page);
+function removeCss(event){
+  event.menu.childNodes.remove(connectCss)
+}
 
-const fileName = location.href;
-console.log(fileName);
-const find = location.href.parentElement;
-console.log(find);
+menu.addEventListener("mouseover", removeCss);
+
+console.log(menu)
+console.dir(removeCss)
 /*
-function changeCss(mouseover) {
-  const currentBanner = document.querySelector(`.${connectBanner}`);
-  if (mouseover) {
-    connectCss.classList.add(currentBanner);
-  } else {
-    connectCss.classList.add(currentBanner);
-  }
-}
-function removeCss(mouseover) {
-  const currentBanner = document.querySelector(`.${connectBanner}`);
-  if (mouseover) {
-    connectCss.classList.remove(currentBanner);
-  } else {
-    connectCss.classList.remove(currentBanner);
-  }
-}
-
-connectCss.addEventListener("mouseover", changeCss);
-connectCss.addEventListener("mouseover", removeCss);
-
-내가 a랭크를 클릭하면
-
-내가 접속해 있는 html 파일의 부모엘리먼트에 connetBanner를 추가
-다른 곳에 감지되면 remove
-
-접속화면의 html 정보 알아서 그 화면에 있는 곳은 connectBanner를 추가해라
-다른 곳에 mouseover가 감지되면 connectBanner를 remove해라*/
+메뉴 html에 기본으로 css 추가 한 다음 그걸 지우는 자바스크립트 작성
+마우스 호버시 배너 class를 삭제 그리고 다시 추가하지 않음
+*/
